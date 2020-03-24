07ebc7370173f364ff8998c07bc5e67753321ec1
AkisArou <akisarou90@gmail.com>
2020-03-24 23:23:00 +0200

    Changed exports

--- lib/Eventing/ENTIRE_STORE_LISTENERS.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const ENTIRE_STORE_LISTENERS: unique symbol;
-//# sourceMappingURL=ENTIRE_STORE_LISTENERS.d.ts.map
--- lib/hoc/withStore.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Store } from "../Store/Store";
-export interface InjectedStoreProps<S> {
-    storeData?: S;
-}
-declare const withStore: <S extends object>(store: Store<S>, dynamicProps?: typeof import("..").ENTIRE_STORE_LISTENERS | (keyof S)[] | keyof S | null | undefined, withEffects?: boolean | undefined, listen?: boolean) => <T extends InjectedStoreProps<S> = InjectedStoreProps<S>>(WrappedComponent: React.ComponentType<T>) => void;
-export { withStore };
-//# sourceMappingURL=withStore.d.ts.map
