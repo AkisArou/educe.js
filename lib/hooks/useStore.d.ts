4d92a868e63173c7ce006d2461250b63f305fa45
AkisArou <akisarou90@gmail.com>
2020-09-03 15:19:55 +0300

    Added: 1)Store.Managed decorator as static unique identifier adder(_storeIdentifier). 2)Store._storeIdentifier for inference Changed: 1)useStore omitted props 2)Store.get and all statics

--- lib/hooks/useStore.d.ts
+++ lib/hooks/useStore.d.ts
@@ -1,6 +1,6 @@
 import { Store } from "../Store/Store";
 import { ENTIRE_STATE } from "../constants/ENTIRE_STATE";
-declare type OmittedProps = "state" & Pick<Store<any>, "requestCleanup" | "requestEffect" | "immutableState" | "subscribe" | "unsubscribe">;
+declare type OmittedProps = "state" | keyof Pick<Store<any>, "requestCleanup" | "requestEffect" | "immutableState" | "subscribe" | "unsubscribe">;
 export declare type SubProps<IStoreType> = (keyof IStoreType)[] | keyof IStoreType | typeof ENTIRE_STATE | null;
 declare function useStore<S extends object, StoreClass extends new (...args: any[]) => Store<S>>(store: (new () => Store<S>) | StoreClass, dynamicProps?: SubProps<S>, withEffects?: boolean, listen?: boolean): readonly [S, Omit<InstanceType<StoreClass>, OmittedProps>];
 declare function useStore<S extends object, StoreClass extends new (...args: any[]) => Store<S>>(store: Store<S>, dynamicProps?: SubProps<S>, withEffects?: boolean, listen?: boolean): S;
